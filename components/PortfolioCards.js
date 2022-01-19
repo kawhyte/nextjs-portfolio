e694bec717d277a09064f5ed369a17f8ea9b9c52
Kenny Whyte <kawhyte@gmail.com>
2022-01-19 09:11:00 -0800

    center portfolio items

--- components/PortfolioCards.js
+++ components/PortfolioCards.js
@@ -6,8 +6,8 @@ import { motion, transform } from "framer-motion";
 
 const Cards = ({ items, hideLastItemOnMobile = false }) => {
 	return (
-		<div className='container mx-auto max-w-7xl'>
-			<div className='grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 '>
+		<div className='container flex justify-center mx-auto max-w-7xl'>
+			<div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 '>
 				{items.map((item) => (
 					<motion.div
 						whileHover={{
