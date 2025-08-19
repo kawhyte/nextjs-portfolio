6bb0f29938f7d3a7511fa21ef883a835ab51f9c3
Kenny Whyte <kawhyte@gmail.com>
2025-08-19 09:40:34 -0700

    feat: add page transitions and fix scrollbar layout shift

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,22 @@ import '../styles/globals.css';
 import Layout from '../components/Layout'; 
 import type { AppProps } from 'next/app'; 
 import 'highlight.js/styles/github-dark.css';
+import { motion, AnimatePresence } from 'framer-motion';
 
-
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps, router }: AppProps) {
   return (
-    
     <Layout>
-      <Component {...pageProps} />
+      <AnimatePresence mode="wait">
+        <motion.div
+          key={router.route}
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: 20 }}
+          transition={{ duration: 0.4, ease: 'easeInOut' }}
+        >
+          <Component {...pageProps} />
+        </motion.div>
+      </AnimatePresence>
     </Layout>
   );
 }
