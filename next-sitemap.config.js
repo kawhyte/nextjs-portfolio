2c7ac58abd34316d104bf4c709abe78ea95ecd09
Kenny Whyte <kawhyte@gmail.com>
2025-08-19 10:35:51 -0700

    feat(seo): add next-sitemap for automatic sitemap generation

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.SITE_URL || 'https://www.kennywhyte.com/',
+  generateRobotsTxt: true, // (Optional)
+  // ...other options
+}
